b8879449493af7e533d76817a9f35f7bc4e387c6
Cristian Pintor <43148727+cpintor@users.noreply.github.com>
2023-09-05 09:03:25 -0500

    done with arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -113,30 +113,76 @@
 ///////////////////////////////////////////////
 // Coding Exercise 5: CHALLENGE #1
 //////////////////////////////////////////////
-const calcAverage = (firstScore, secondScore, thirdScore) => {
-  const average = (firstScore + secondScore + thirdScore) / 3;
+// const calcAverage = (firstScore, secondScore, thirdScore) => {
+//   const average = (firstScore + secondScore + thirdScore) / 3;
 
-  return average;
+//   return average;
+// };
+
+// // Test 1
+// let scoreDolphins = calcAverage(44, 23, 71);
+// let scoreKoalas = calcAverage(65, 54, 49);
+
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//   if (avgKoalas > 2 * avgDolphins) {
+//     console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+//   } else if (avgDolphins > 2 * avgKoalas) {
+//     console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+//   } else {
+//     console.log('No team wins...');
+//   }
+// };
+
+// checkWinner(scoreDolphins, scoreKoalas);
+
+// // Test 2
+// scoreDolphins = calcAverage(85, 54, 41);
+// scoreKoalas = calcAverage(23, 34, 27);
+
+// checkWinner(scoreDolphins, scoreKoalas);
+
+///////////////////////////////////////////////
+// 39. Introduction to Arrays
+//////////////////////////////////////////////
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+// const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+// Size of array
+console.log(friends.length);
+// Get last item in array
+console.log(friends[friends.length - 1]);
+// Mutate array (replace)
+friends[2] = 'Jay';
+console.log(friends);
+
+// Array with different stuff
+const firstName = 'Jonas';
+const jonas = [firstName, 'Stuart', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
+
+// Array exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
 };
 
-// Test 1
-let scoreDolphins = calcAverage(44, 23, 71);
-let scoreKoalas = calcAverage(65, 54, 49);
+const years = [1990, 1967, 2002, 2010, 2018];
 
-const checkWinner = function (avgDolphins, avgKoalas) {
-  if (avgKoalas > 2 * avgDolphins) {
-    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
-  } else if (avgDolphins > 2 * avgKoalas) {
-    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
-  } else {
-    console.log('No team wins...');
-  }
-};
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
 
-checkWinner(scoreDolphins, scoreKoalas);
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
 
-// Test 2
-scoreDolphins = calcAverage(85, 54, 41);
-scoreKoalas = calcAverage(23, 34, 27);
-
-checkWinner(scoreDolphins, scoreKoalas);
+console.log(ages);
