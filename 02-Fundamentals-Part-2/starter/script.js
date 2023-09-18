7688815041090b3bb07c11f9fe209cc1109e38bc
Cristian Pintor <43148727+cpintor@users.noreply.github.com>
2023-09-17 19:45:31 -0500

    Commenting sections 42 and 43

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -255,47 +255,47 @@
 // 43. DOT VS. BRACKET NOTATION
 //////////////////////////////////////////////
 
-// How retrieve data from objects
-const jonas = {
-  firstName: 'Jonas',
-  lastName: 'Steven',
-  age: 2037 - 1991,
-  job: 'teacher',
-  friennds: ['Michael', 'Peter', 'Steven'],
-};
+// // How retrieve data from objects
+// const jonas = {
+//   firstName: 'Jonas',
+//   lastName: 'Steven',
+//   age: 2037 - 1991,
+//   job: 'teacher',
+//   friennds: ['Michael', 'Peter', 'Steven'],
+// };
 
-// 1. Dot-notation
-console.log(jonas.lastName);
+// // 1. Dot-notation
+// console.log(jonas.lastName);
 
-// 2. Bracket-notation
-console.log(jonas['lastName']);
+// // 2. Bracket-notation
+// console.log(jonas['lastName']);
 
-// Get first and last name
-const nameKey = 'Name';
-console.log(jonas['first' + nameKey]);
-console.log(jonas['last' + nameKey]);
+// // Get first and last name
+// const nameKey = 'Name';
+// console.log(jonas['first' + nameKey]);
+// console.log(jonas['last' + nameKey]);
 
-// Access object property
-const interestedIn = prompt(
-  'What do you want to know about Jonas? Choose between firstName, lastName, age, and friends.'
-);
-// console.log(jonas[interestedIn]);
+// // Access object property
+// const interestedIn = prompt(
+//   'What do you want to know about Jonas? Choose between firstName, lastName, age, and friends.'
+// );
+// // console.log(jonas[interestedIn]);
 
-if (jonas[interestedIn]) {
-  console.log(jonas[interestedIn]);
-} else {
-  console.log(
-    'Wrong request! Choose between firstName, lastName, age, and friends.'
-  );
-}
+// if (jonas[interestedIn]) {
+//   console.log(jonas[interestedIn]);
+// } else {
+//   console.log(
+//     'Wrong request! Choose between firstName, lastName, age, and friends.'
+//   );
+// }
 
-// Use dot and bracket notation to add new properties to the object
-jonas.location = 'Portugal';
-jonas['twitter'] = '@jonassteven';
-console.log(jonas);
+// // Use dot and bracket notation to add new properties to the object
+// jonas.location = 'Portugal';
+// jonas['twitter'] = '@jonassteven';
+// console.log(jonas);
 
-// Challenge
-// "Jonas has 3 friends, and his best friend is called Michael"
-console.log(
-  `${jonas.firstName} has ${jonas.friennds.length} friends, and his best friend is called ${jonas.friennds[0]}`
-);
+// // Challenge
+// // "Jonas has 3 friends, and his best friend is called Michael"
+// console.log(
+//   `${jonas.firstName} has ${jonas.friennds.length} friends, and his best friend is called ${jonas.friennds[0]}`
+// );
