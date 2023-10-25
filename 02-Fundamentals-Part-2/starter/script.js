809f7a5bfc716842294a0789aa48e737a1b273d0
Cristian Pintor <43148727+cpintor@users.noreply.github.com>
2023-10-25 17:12:19 -0500

    Finishing Coding Exercise 8: CHALLENGE #4
    See: https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22648309?start=225#overview

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -478,7 +478,7 @@
 // }
 
 ///////////////////////////////////////////////
-// 48. The While Loop
+// 49. The While Loop
 //////////////////////////////////////////////
 
 // For loop as reference
@@ -497,11 +497,48 @@
 // }
 
 // Example
-let dice = Math.trunc(Math.random() * 6) + 1;
-console.log(dice);
+// let dice = Math.trunc(Math.random() * 6) + 1;
+// console.log(dice);
 
-while (dice !== 6) {
-  console.log(`You rolled a ${dice}`);
-  dice = Math.trunc(Math.random() * 6) + 1;
-  if (dice === 6) console.log('Loop is about to end');
+// while (dice !== 6) {
+//   console.log(`You rolled a ${dice}`);
+//   dice = Math.trunc(Math.random() * 6) + 1;
+//   if (dice === 6) console.log('Loop is about to end');
+// }
+
+///////////////////////////////////////////////
+// Coding Exercise 8: CHALLENGE #4
+//////////////////////////////////////////////
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+/* Write your code below. Good luck! 🙂 */
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  // inserting each bill and doing calcTip()
+  tips.push(calcTip(bills[i]));
+  totals.push(bills[i] + tips[i]);
 }
+
+console.log(tips);
+console.log(totals);
+
+// BONUS CHALLENGE
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
