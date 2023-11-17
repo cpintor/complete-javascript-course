8e80f3be7a064135ea755e21322c71997e8022b6
Cristian Pintor <43148727+cpintor@users.noreply.github.com>
2023-11-17 16:18:58 -0600

    73. Handling click events
    See: https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22648411#overview

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+// Event listeners
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number! 😡';
+  }
+});
