7399281f6eb14e7b155a1c7a434b76ad8088e20e
Cristian Pintor <43148727+cpintor@users.noreply.github.com>
2023-11-22 22:13:20 -0600

    80. Working with Classes
    see: https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22648437#content

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,9 +6,25 @@ const btnCloseModal = document.querySelector('.close-modal');
 
 const btsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btsOpenModal);
+const openModal = function () {
+  console.log('Button clicked');
+  // using classList to get .hidden class
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 // loop over btsOpenModal buttons
+// open model when clicking buttons
 for (let i = 0; i < btsOpenModal.length; i++) {
-  console.log(btsOpenModal[i].textContent);
+  btsOpenModal[i].addEventListener('click', openModal);
 }
+
+// Ability to close modal and remove overlay
+// closeModal function is only triggered when the click event fires
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
