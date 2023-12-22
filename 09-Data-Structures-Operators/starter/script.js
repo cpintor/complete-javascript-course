699cb8fd08cadad5193528981fe678be2d1ef0dd
Cristian Pintor <43148727+cpintor@users.noreply.github.com>
2023-12-21 21:24:31 -0600

    103. Destructuring Arrays
    See: https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22648521?start=930#notes

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,18 +12,47 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 };
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// Destructuring the array as opposed to the a,b,c above
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+// Getting first and third elements from restaurant.categories
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// Switching variables using temp variable
+// const temp = main; // storing 'Italian'
+// main = secondary; // Italian = Vegetarian
+// secondary = temp; // Vegetarian = Italian
+// console.log(main, secondary);
+
+// Switching variables using destructuring
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// Recieve 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// Destructure a nested array
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+// Destruct inner array
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
