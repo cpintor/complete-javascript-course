ad575b0be0f2ca94e200be298ffb7d98ad8034c5
Cristian Pintor <43148727+cpintor@users.noreply.github.com>
2024-10-25 18:03:11 -0500

    110. Logical Assignment Operators

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -303,3 +303,38 @@ const restaurant = {
 // // Nullish: null and undefined (not or '')
 // const guestCorrect = restaurant.numGuests ?? 10;
 // console.log(guestCorrect);
+
+/***********************************/
+// 110. Logical Assignment Operators
+/***********************************/
+
+// const rest1 = {
+//   name: 'Capri1',
+//   // numGuests: 20,
+//   numGuests: 0,
+// };
+
+// const rest2 = {
+//   name: 'La Piazza',
+//   owner: 'Giovannu Rossi',
+// };
+
+// // OR assignment operator
+// // rest1.numGuests = rest1.numGuests || 10;
+// // rest2.numGuests = rest2.numGuests || 10;
+
+// // rest1.numGuests ||= 10; // Ecxactly the same as code above
+// // rest2.numGuests ||= 10;
+
+// // nullish assignment operator (null or undefined)
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
+
+// // AND assignment operator
+// // rest1.owner = rest1.owner && '<ANONYMOUS>';
+// // rest2.owner = rest2.owner && '<ANONYMOUS>';
+// rest1.owner &&= '<ANONYMOUS>';
+// rest2.owner &&= '<ANONYMOUS>';
+
+// console.log(rest1);
+// console.log(rest2);
