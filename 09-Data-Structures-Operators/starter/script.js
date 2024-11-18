7c4a58d8eae6f63e7b3681b63eb855d4d658546c
Cristian Pintor <43148727+cpintor@users.noreply.github.com>
2024-11-18 13:33:50 -0600

    CHALLENGE #4

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -963,3 +963,25 @@ whether it's in the first half or second half (after 45 min) of the game, like t
 // planesInLine(5);
 // planesInLine(3);
 // planesInLine(12);
+
+/*************************************/
+// CHALLENGE #4
+/*************************************/
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
+
+// document.querySelector('button').addEventListener('click', function () {
+//   const text = document.querySelector('textarea').value;
+//   // splitting string into array
+//   const rows = text.split('\n');
+//   console.log(rows);
+
+//   for (const [i, row] of rows.entries()) {
+//     const [first, second] = row.toLowerCase().trim().split('_');
+//     const output = `${first}${second.replace(
+//       second[0],
+//       second[0].toUpperCase()
+//     )}`;
+//     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+//   }
+// });
