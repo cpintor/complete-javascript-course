5fb812cf8b7f9a5acd49c8df7bcd18ce67bbebe4
Cristian Pintor <43148727+cpintor@users.noreply.github.com>
2024-11-19 15:20:30 -0600

    129. Default Parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+/*************************************/
+// 129. Default Parameters
+/*************************************/
+
+// const bookings = [];
+
+// // ES6: Setting default values in argumment
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers // expressions and be used
+// ) {
+//   // ES5: setting numPassengers and price default
+//   //   numPassengers = numPassengers || 1;
+//   //   price = price || 199;
+
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
+// createBooking('LH123', undefined, 1000); // passing undefined to numPassengers
