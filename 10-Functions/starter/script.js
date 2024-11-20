ec4259b578229fc726ae7d8719b3a7a9db7e0090
Cristian Pintor <43148727+cpintor@users.noreply.github.com>
2024-11-20 14:25:31 -0600

    130. How Passing Arguments Works: Value vs. Reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -31,3 +31,41 @@
 // createBooking('LH123', 2);
 // createBooking('LH123', 5);
 // createBooking('LH123', undefined, 1000); // passing undefined to numPassengers
+
+/*************************************/
+// 130. How Passing Arguments Works: Value vs. Reference
+/*************************************/
+// JS only passes by value NOT by reference
+
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Schmedtmann',
+//   passport: 24739479284,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'Lh999';
+//   passenger.name = 'Mr. ' + passenger.name;
+
+//   if (passenger.passport === 24739479284) {
+//     alert('Check in');
+//   } else {
+//     alert('Wrong passport');
+//   }
+// };
+
+// // checkIn(flight, jonas);
+// // console.log(flight);
+// // console.log(jonas);
+
+// // It is the same as doing:
+// // const flightNum = flight;
+// // const passenger = jonas;
+
+// // Will change person's passport
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 100000000000);
+// };
+
+// newPassport(jonas);
+// checkIn(flight, jonas);
