a12573d85ae67959f1875a4a0bf0ea1bdf6ee3cc
Cristian Pintor <43148727+cpintor@users.noreply.github.com>
2024-12-04 17:27:35 -0600

    148. Creating DOM Elements

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -178,3 +178,30 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // currenciesUnique.forEach(function (value, key, map) {
 //   console.log(`${key}: ${value}`);
 // });
+
+/*****************************************************/
+// 148. Creating DOM Elements
+/*****************************************************/
+// Display movements from accounts
+// const displayMovements = function (movements) {
+//   // Emptying movements container
+//   containerMovements.innerHTML = '';
+
+//   movements.forEach(function (mov, i) {
+//     const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+//     // Displaying movements
+//     const html = `
+//       <div class="movements__row">
+//         <div class="movements__type movements__type--${type}">${
+//       i + 1
+//     } ${type}</div>
+//         <div class="movements__value">${mov}</div>
+//       </div>
+//     `;
+
+//     // Adding new child elements right after movement element
+//     containerMovements.insertAdjacentHTML('afterbegin', html);
+//   });
+// };
+// displayMovements(account1.movements);
