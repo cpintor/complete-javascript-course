fb38ca979f9b02e9666169ae325b767d0d843afc
Cristian Pintor <43148727+cpintor@users.noreply.github.com>
2024-12-05 15:51:57 -0600

    153. The filter method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -290,3 +290,26 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 // createUsernames(accounts);
 // console.log(accounts);
+
+/*****************************************************/
+// 153. The filter method
+/*****************************************************/
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// // Using filter()
+// const deposits = movements.filter(function (mov) {
+//   return mov > 0;
+// });
+
+// console.log(movements);
+// console.log(deposits);
+
+// // Using for of loop
+// const depositsFor = [];
+// for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+// console.log(depositsFor);
+
+// // Using arrow function
+// const withdrawals = movements.filter(mov => mov < 0);
+
+// console.log(withdrawals);
