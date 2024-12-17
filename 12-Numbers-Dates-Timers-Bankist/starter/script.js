8a5eafae05c2d887ca883a9b8fd655885610aa39
Cristian Pintor <43148727+cpintor@users.noreply.github.com>
2024-12-17 15:05:24 -0600

    179. Converting and Checking Numbers

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +inputLoanAmount.value;
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -251,3 +251,41 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+/*****************************************************/
+// 179. Converting and Checking Numbers
+/*****************************************************/
+
+// console.log(23 === 23.0);
+
+// // Difficult to represent in base 2
+// console.log(0.1 + 0.2); // results in incorrect number
+// console.log(0.1 + 0.2 === 0.3); // should result as true
+
+// // Conversion
+// console.log(Number('23'));
+// console.log(+'23'); // another way to convert string to number without using Number()
+
+// // Parsing
+// console.log(Number.parseInt('30px', 10)); // parseInt() finds number in string but string needs to start w a number
+// console.log(Number.parseInt('e23', 10));
+
+// console.log(Number.parseInt(' 2.5rem '));
+// console.log(Number.parseFloat(' 2.5rem '));
+
+// // isNan() - check if value is not a number NaN
+// console.log(Number.isNaN(20));
+// console.log(Number.isNaN('20'));
+// console.log(Number.isNaN(+'20X'));
+// console.log(Number.isNaN(23 / 0)); // results in 'false'
+
+// // isFinite() - best way of checking if value is a number
+// console.log(Number.isFinite(20));
+// console.log(Number.isFinite('20'));
+// console.log(Number.isFinite(+'20X'));
+// console.log(Number.isFinite(23 / 0));
+
+// // isInteger()
+// console.log(Number.isInteger(23));
+// console.log(Number.isInteger(23.0));
+// console.log(Number.isInteger(23 / 0));
