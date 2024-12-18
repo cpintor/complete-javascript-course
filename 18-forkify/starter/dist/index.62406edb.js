73e31d3519fb46df4544df22381a8bfb58fc52db
Cristian Pintor <43148727+cpintor@users.noreply.github.com>
2024-12-18 13:50:49 -0600

    299. Rendering the Recipe

---
+++ 18-forkify/starter/dist/index.62406edb.js
@@ -0,0 +1,36 @@
+const recipeContainer = document.querySelector('.recipe');
+const timeout = function(s) {
+    return new Promise(function(_, reject) {
+        setTimeout(function() {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
+};
+// https://forkify-api.jonas.io/
+///////////////////////////////////////
+// Async function runs in the background
+const showRecipe = async function() {
+    try {
+        const res = await fetch('https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e8abd');
+        const data = await res.json();
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        // removing underscores from API
+        let { recipe } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceURL: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        };
+        console.log(recipe);
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
+
+//# sourceMappingURL=index.62406edb.js.map
--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -8,6 +8,36 @@ const timeout = function (s) {
   });
 };
 
-// https://forkify-api.herokuapp.com/v2
+// https://forkify-api.jonas.io/
 
 ///////////////////////////////////////
+// Async function runs in the background
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e8abd'
+      // 'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    // removing underscores from API
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceURL: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
